f85b77710c52bb5437d35fb4d551ac22f92149d2
Ernest Walzel <ewalzel@gmail.com>
2020-03-27 13:35:34 +0100

    Add Images import

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Building extends Model
 {
-    //
+    public function images()
+    {
+        return $this->hasMany('App\Models\Image');
+    }
 }
---
+++ database/migrations/2020_03_27_115558_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->integer('source_id')->unique();
+
+            $table->foreignId('building_id')->constrained();
+            $table->string('title');
+            $table->string('author')->nullable();
+            $table->string('created_date')->nullable();
+            $table->string('source')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
