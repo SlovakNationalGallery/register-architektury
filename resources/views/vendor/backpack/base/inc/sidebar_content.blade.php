3b7a0f4c029df05962433151c4bfcccccde3ef1d
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-04-09 12:29:18 +0200

    add backpack with architects, buildings and images

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,5 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('architect') }}'><i class='nav-icon la la-user-alt'></i> Architects</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('building') }}'><i class='nav-icon la la-building'></i> Buildings</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('image') }}'><i class='nav-icon la la-image'></i> Images</a></li>
