23f0e4b4e5a86d4d3ef6d6b2db151a2b2cf97a67
Ernest Walzel <ewalzel@gmail.com>
2020-04-16 10:30:13 +0200

    Add slug to Publication

--- database/migrations/2020_04_10_131458_create_publications.php
+++ database/migrations/2020_04_10_131458_create_publications.php
@@ -16,7 +16,7 @@ class CreatePublications extends Migration
             $table->increments('id');
 
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('authors');
             $table->string('cover_image')->nullable();
             $table->text('description')->nullable();
