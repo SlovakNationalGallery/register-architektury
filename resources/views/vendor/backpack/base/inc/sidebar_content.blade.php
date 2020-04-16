385aef89174edf268a7b7257f3fa751bbded8af5
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-16 17:54:41 +0200

    Update sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,13 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('articles') }}'><i class='nav-icon la la-newspaper-o'></i> Articles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('publications') }}'><i class='nav-icon la la-book'></i> Publications</a></li>
+
+<li class="nav-title">Naimportované</li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('architect') }}'><i class='nav-icon la la-user-alt'></i> Architects</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('building') }}'><i class='nav-icon la la-building'></i> Buildings</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('image') }}'><i class='nav-icon la la-image'></i> Images</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('publications') }}'><i class='nav-icon la la-book'></i> Publications</a></li>
 
+<li class="nav-title">Ostatné</li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+
