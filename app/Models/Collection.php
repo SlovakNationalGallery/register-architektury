ad72c3a51f22ad22e543cd36b06bab4f76f86b26
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-17 11:36:23 +0200

    Collection model and migration

---
+++ app/Models/Collection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use App\Traits\Publishable;
+
+class Collection extends Model
+{
+    use CrudTrait, HasSlug, Publishable;
+
+    protected $guarded = ['id'];
+    protected $dates = ['published_at'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function buildings()
+    {
+        return $this->belongsToMany('App\Models\Building');
+    }
+}
