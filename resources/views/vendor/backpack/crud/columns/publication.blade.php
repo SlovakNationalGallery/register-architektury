8b4261e316bffd42484f3dd80c9b93db21b8d3ad
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-23 11:42:18 +0200

    Handle articles and publications with no cover images

--- resources/views/vendor/backpack/crud/columns/publication.blade.php
+++ resources/views/vendor/backpack/crud/columns/publication.blade.php
@@ -2,9 +2,11 @@
     $edit_url = url($crud->route.'/'.$entry->getKey().'/edit');
 @endphp
 <div class="media" style="white-space: normal">
+    @if($entry->cover_image)
     <a href="{{ $edit_url }}">
         <img class="mr-3" src="{{ Storage::url($entry->cover_image) }}" width="80">
     </a>
+    @endif
     <div class="media-bod small">
         <h5 class="mt-0"><a href="{{ $edit_url }}">{{ $entry->title }}</a></h5>
         {{ Str::limit(strip_tags($entry->description), 120) }}
