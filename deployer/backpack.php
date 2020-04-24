973e6dc50312b13850f8b6f4bd2cc28b487cb353
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-24 10:29:52 +0200

    Add elfinder:publish to deployer tasks

--- deployer/backpack.php
+++ deployer/backpack.php
@@ -5,4 +5,5 @@ namespace Deployer;
 desc('Install Backpack dependencies');
 task('backpack:install', function () {
     run('{{bin/php}} {{release_path}}/artisan backpack:install -q');
+    run('{{bin/php}} {{release_path}}/artisan elfinder:publish');
 })->once();
