4ed31373053d1f0b513b6e2e803c30e6f759989e
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-30 08:45:05 +0200

    Enable Telescope in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+        //
     }
 
     /**
