583044b5ebb10d6a7890b90db52865c4aff1193f
Ernest Walzel <ernest.walzel@sng.sk>
2020-04-30 13:53:02 +0200

    Remove comment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('telescope:prune --hours=72')->daily();
-        // $schedule->command('inspire')->hourly();
     }
 
     /**
