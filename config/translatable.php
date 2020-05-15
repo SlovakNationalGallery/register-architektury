7321ff40b8b4cd3c7b9508de91ed58b8be98a6f0
Ernest Walzel <ernest.walzel@sng.sk>
2020-05-15 15:20:03 +0200

    Add spatie/laravel-translatable

---
+++ config/translatable.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    /*
+     * If a translation has not been set for a given locale, use this locale instead.
+     */
+    //TODO this file can be deleted after REGARCH-18 is complete and we can set app fallback_locale to 'sk'
+    'fallback_locale' => 'sk',
+];
