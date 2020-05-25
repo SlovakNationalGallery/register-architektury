205fd4d1f3c4d034ff3e581ed4158607f27f602a
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-05-25 10:48:52 +0200

    add header component with translations

---
+++ resources/lang/sk/header.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Register modernej architektúry oA HÚ SAV',
+    'oa' => 'oA',
+    'architects' => 'architekti',
+    'objects' => 'objekty',
+    'collections' => 'kolekcie',
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,40 +2,10 @@
 @section('title', __('app.title'))
 
 @section('content')
-<div class="container-fluid p-0">
-    <div class="row no-gutters align-items-stretch">
-        <div class="col-md-4 py-3 border border-dark text-center align-items-center">
-            <h1 class="ls-2 my-auto">Register modernej architektúry oA HÚ SAV</h1>
-        </div>
-        <div class="col-md-4 border border-dark text-center d-flex">
-            <form class="px-3 my-auto w-100">
-                <input type="text" name="search" class="form-control form-control-sm" value="{{ request('search') }}">
-            </form>
-        </div>
-        <div class="col-md-4 py-3 border border-dark text-center">
-            @include('components.langswitch')
-        </div>
-    </div>
-    <div class="row no-gutters">
-        <div class="col-md-12">
-            <nav class="nav nav-justified main-nav">
-                <a class="nav-item py-3 ls-2 border border-dark nav-link" href="#">oA</a>
-                <a class="nav-item py-3 ls-2 border border-dark text-uppercase nav-link" href="#">architekti</a>
-                <a class="nav-item py-3 ls-2 border border-dark text-uppercase nav-link" href="#">objekty</a>
-                <a class="nav-item py-3 ls-2 border border-dark text-uppercase nav-link" href="#">kolekcie</a>
-            </nav>
-        </div>
-    </div>
-    <div class="row no-gutters">
-        <div class="col-md-12">
-            <nav class="nav nav-justified sub-nav">
-                <a class="nav-item py-3 ls-3 border border-dark nav-link" href="#">MoMoWo</a>
-                <a class="nav-item py-3 ls-3 border border-dark text-uppercase nav-link" href="#">ATRIUM</a>
-                <a class="nav-item py-3 ls-3 border border-dark nav-link" href="#">ŠUR</a>
-                <a class="nav-item py-3 ls-3 border border-dark nav-link" href="#">Do.co,mo.mo</a>
-            </nav>
-        </div>
-    </div>
+
+@include('components.header')
+
+<div class="container-fluid p-0">	
     <div class="row no-gutters">
         <div class="col-md-12 border border-dark p-3">
             <div class="row no-gutters">
