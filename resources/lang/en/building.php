b9c2c1ea042617331bc78dab279a70d941366a8f
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-05-28 17:08:05 +0200

    add en translation strings for object detail

---
+++ resources/lang/en/building.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'current_function' => 'type',
+    'address' => 'adress',
+    'architects' => 'architects',
+    'builders' => 'builders',
+    'competition_date' => 'competition',
+    'project_date' => 'project',
+    'bibliography' => 'Bibliography',
+];
