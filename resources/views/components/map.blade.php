80ea1fa6b7fa2620ebe452146503798df842c46b
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-06-05 15:15:47 +0200

    add lang files for map component

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -2,7 +2,7 @@
     <div class="position-absolute bg-light border-bottom vh-38 w-100 {{ ($show_map) ? '' : 'hide' }}" id="map" data-location="{{ $location }}" data-location="" data-center="" data-zoom=""></div>
     <div class="row no-gutters">
     	  <div class="col-sm-4 order-sm-2 text-sm-right z-index-1 p-3">
-        	  <button class="btn btn-outline-dark btn-lg mb-2" id="map-toggle">MAPA</button>
+        	  <button class="btn btn-outline-dark btn-lg mb-2 text-uppercase" id="map-toggle">{{ __('map.map') }}</button>
      	  </div>
         <div class="col-sm-8 order-sm-1 z-index-1 p-3">
             @include('components.tags', ['tags' => $tags])
