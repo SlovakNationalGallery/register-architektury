e4e1a1f9a7562583a9b98812236a7f7fbad7f574
Ernest Walzel <ernest.walzel@sng.sk>
2020-06-05 15:36:00 +0200

    Basic MediaLibrary setup

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Image extends Model
+class Image extends Model implements HasMedia
 {
     use CrudTrait;
+    use InteractsWithMedia;
 
     public function building()
     {
