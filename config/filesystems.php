264a3154bedc383121fa951e6a55914d250e23cf
Ernest Walzel <ernest.walzel@sng.sk>
2020-06-05 15:54:03 +0200

    Store images in storage/app/public/images

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,6 +69,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'public_images' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/images'),
+            'url' => env('APP_URL').'/storage/images',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
