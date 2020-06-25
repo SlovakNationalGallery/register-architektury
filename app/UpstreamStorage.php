7ac2aa088ff9d1f68e24147c2aeabc6dfefd8bdb
Ernest Walzel <ernest.walzel@sng.sk>
2020-06-25 14:46:11 +0200

    Extract "UpstreamStorage" path logic

---
+++ app/UpstreamStorage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Exception;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class UpstreamStorage
+{
+    public static function get($sourcePath)
+    {
+        $storage = Storage::disk(Config('filesystems.upstream'));
+        $normalizedPath = Str::of($sourcePath)->replace('\\', '/');
+
+        if (Str::startsWith($normalizedPath, '//AA20/OddArch/AA20 dáta/')) {
+            return $storage->get($normalizedPath->replaceFirst('//AA20/OddArch/AA20 dáta/',''));
+        }
+
+        if (Str::startsWith($normalizedPath, 'S:/AA20 dáta/')) {
+            return $storage->get($normalizedPath->replaceFirst('S:/AA20 dáta/',''));
+        }
+
+        throw new Exception("Unrecognized source path $sourcePath");
+    }
+}
