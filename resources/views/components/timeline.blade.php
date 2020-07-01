c66ec076e5d69dddd7f2c4f6524dfa3493c61692
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-07-01 15:00:39 +0200

    make timeline component

---
+++ resources/views/components/timeline.blade.php
@@ -0,0 +1,3 @@
+<div id="years" class="my-4 mx-4"></div>
+<input type="hidden" name="year_from" id="year_from" value="{{ $year_from }}" data-min="{{ $year_min }}">
+<input type="hidden" name="year_until" id="year_until" value="{{ $year_until }}" data-max="{{ $year_max }}">
