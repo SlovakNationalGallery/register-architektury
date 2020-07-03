4b2ec64cc4fa8df1662f60a1f9ec061b1ba492d9
Ernest Walzel <ernest.walzel@sng.sk>
2020-07-03 09:21:02 +0200

    Make Flickity work after 'load more' append

--- resources/js/components/load-more.js
+++ resources/js/components/load-more.js
@@ -1,15 +1,24 @@
-const InfiniteScroll = require('infinite-scroll');
+const InfiniteScroll = require("infinite-scroll");
+const Flickity = require('flickity');
 
-$(document).ready(function(){
-	if($('.pagination__next').length > 0) {
-		var infScroll = new InfiniteScroll( '.items', {
-			loadOnScroll: false,
-			path: '.pagination__next',
-			append: '.item',
-			history: 'replace',
-			status: '.page-load-status',
-			button: '.view-more-button',
-			scrollThreshold: false,
-		});
-	}
+$(document).ready(function() {
+    if ($(".pagination__next").length > 0) {
+        var infScroll = new InfiniteScroll(".items", {
+            loadOnScroll: false,
+            path: ".pagination__next",
+            append: ".item",
+            history: "replace",
+            status: ".page-load-status",
+            button: ".view-more-button",
+            scrollThreshold: false
+        });
+
+        infScroll.on('append', function(response, path, items) {
+
+            // Hydrate new flickity carousels
+            $(items).find('[data-flickity]').each(function(index, element) {
+                new Flickity(element, $(element).data('flickity'))
+            })
+        })
+    }
 });
