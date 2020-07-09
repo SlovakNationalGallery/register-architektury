336e26c69d75fe584fbb38b15885194d88b136df
Ernest Walzel <ernest.walzel@sng.sk>
2020-07-09 09:07:33 +0200

    Fix bug in sorting components that would somehow modify the request object

--- resources/views/components/architects-sort-by.blade.php
+++ resources/views/components/architects-sort-by.blade.php
@@ -1,3 +1,3 @@
 <span class="">{{ $architects->total() }} {{ trans_choice('architect.count', $architects->total()) }}</span>
-<a href="{{ route('architects.index', request()->merge(['sort_by' => 'oldest'])->all()) }}" class="link-no-underline ml-5">{{ __('architect.sort.oldest') }} &darr;</a>
-<a href="{{ route('architects.index', request()->merge(['sort_by' => 'newest'])->all()) }}" class="link-no-underline ml-5">{{ __('architect.sort.newest') }} &uarr;</a>
+<a href="{{ request()->fullUrlWithQuery(['sort_by' => 'oldest']) }}" class="link-no-underline ml-5">{{ __('architect.sort.oldest') }} &darr;</a>
+<a href="{{ request()->fullUrlWithQuery(['sort_by' => 'newest']) }}" class="link-no-underline ml-5">{{ __('architect.sort.newest') }} &uarr;</a>
