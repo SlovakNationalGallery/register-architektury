0ba07fe2b6eb7af8dd54f66a335ed3366f3d7987
Ernest Walzel <ernest.walzel@sng.sk>
2020-07-10 10:33:57 +0200

    Add pictures and use for-loop

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -5,8 +5,8 @@
 
 @include('components.header')
 
-<div class="container-fluid px-4 pt-4">
-    <div class="row pt-lg-7">
+<div class="container-fluid px-4 pt-4 pt-lg-7">
+    <div class="row">
         <div class="col-lg-3 d-flex align-items-end">
             <p class="mb-4 mb-lg-0">{{ __('app.oa') }} {{ __('app.hu') }}<br/>{{ __('app.sav') }}</p>
         </div>
@@ -15,50 +15,47 @@
         </div>
     </div>
 </div>
-<div class="container-fluid mt-5 mt-lg-3 pb-4">
+<div class="container-fluid mt-5 mt-lg-3 pb-4 pb-lg-7">
+    @php
+    $people = [
+        (object) [
+            'name' => 'prof. Dr. Ing. arch. Henrieta Moravčíková',
+            'image_url' => asset('images/henrieta-moravcikova.jpg'),
+            'role' => __('about.department.head_of_department'),
+            'email' => 'henrieta.moravcikova@savba.sk',
+        ],
+        (object) [
+            'name' => 'PhDr. Katarína Haberlandová, PhD.',
+            'image_url' => asset('images/katarina-haberlandova.jpg'),
+            'role' => __('about.department.researcher_female'),
+            'email' => 'katarina.haberlandova@savba.sk',
+        ],
+        (object) [
+            'name' => 'Peter Szalay, PhD.',
+            'image_url' => asset('images/peter-szalay.jpg'),
+            'role' => __('about.department.researcher_male'),
+            'email' => 'peter.szalay@savba.sk',
+        ],
+        (object) [
+            'name' => 'Ing. arch. Laura Pastoreková Krišteková, PhD.',
+            'image_url' => asset('images/laura-pastorekova.jpg'),
+            'role' => __('about.department.researcher_female'),
+            'email' => 'laura.pastorekova@savba.sk'
+        ],
+    ];
+    @endphp
+    @foreach($people as $person)
     <div class="row border-top py-3">
         <div class="col-8 col-lg-3 px-4 mb-4 mb-lg-0">
-            <img src="https://placekitten.com/g/204/234" class="mw-100" alt="Henrieta Moravčíková">
+            <img src="{{ $person->image_url }}" class="mw-100" alt="{{ $person->name }}">
         </div>
         <div class="col-lg-3 px-4 d-flex flex-column justify-content-end">
-            <span>{{ __('about.department.head_of_department')}}</span>
-            <h6 class="font-weight-bold">prof. Dr. Ing. arch. Henrieta Moravčíková</h6>
+            <span>{{ $person->role }}</span>
+            <h6 class="font-weight-bold">{{ $person->name }}</h6>
             <address>HÚ SAV, Dúbravská cesta 9, 845 03 Bratislava</address>
-            <span>E-mail: <a href="mailto:henrieta.moravcikova@savba.sk">henrieta.moravcikova@savba.sk</a></span>
+            <span>E-mail: <a href="mailto:{{ $person->email }}">{{ $person->email }}</a></span>
         </div>
     </div>
-    <div class="row border-top py-3">
-        <div class="col-8 col-lg-3 px-4 mb-4 mb-lg-0">
-            <img src="https://placekitten.com/g/204/234" class="mw-100" alt="Katarína Haberlandová">
-        </div>
-        <div class="col-lg-3 px-4 d-flex flex-column justify-content-end">
-            <span>{{ __('about.department.researcher_female')}}</span>
-            <h6 class="font-weight-bold">PhDr. Katarína Haberlandová, PhD.</h6>
-            <address>HÚ SAV, Dúbravská cesta 9, 845 03 Bratislava</address>
-            <span>E-mail: <a href="mailto:katarina.haberlandova@savba.sk">katarina.haberlandova@savba.sk</a></span>
-        </div>
-    </div>
-    <div class="row border-top py-3">
-        <div class="col-8 col-lg-3 px-4 mb-4 mb-lg-0">
-            <img src="https://placekitten.com/g/204/234" class="mw-100" alt="Peter Szalay">
-        </div>
-        <div class="col-lg-3 px-4 d-flex flex-column justify-content-end">
-            <span>{{ __('about.department.researcher_male')}}</span>
-            <h6 class="font-weight-bold">Peter Szalay, PhD.</h6>
-            <address>HÚ SAV, Dúbravská cesta 9, 845 03 Bratislava</address>
-            <span>E-mail: <a href="mailto:peter.szalay@savba.sk">peter.szalay@savba.sk</a></span>
-        </div>
-    </div>
-    <div class="row border-top py-3">
-        <div class="col-8 col-lg-3 px-4 mb-4 mb-lg-0">
-            <img src="https://placekitten.com/g/204/234" class="mw-100" alt="Laura Pastoreková Krišteková">
-        </div>
-        <div class="col-lg-3 px-4 d-flex flex-column justify-content-end">
-            <span>{{ __('about.department.researcher_female')}}</span>
-            <h6 class="font-weight-bold">Ing. arch. Laura Pastoreková Krišteková, PhD.</h6>
-            <address>HÚ SAV, Dúbravská cesta 9, 845 03 Bratislava</address>
-            <span>E-mail: <a href="mailto:laura.pastorekova@savba.sk">laura.pastorekova@savba.sk</a></span>
-        </div>
-    </div>
+    @endforeach
 </div>
 @endsection
