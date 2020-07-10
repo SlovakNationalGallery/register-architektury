24a394bdf27036e83442c5fe5f1bac8737aa306d
Ernest Walzel <ernest.walzel@sng.sk>
2020-07-10 09:24:54 +0200

    Basic 'people' layout with intro text

---
+++ resources/lang/en/about.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'department' => [
+        'intro' => 'Research activities of the Department of architecture are aimed at 20th
+            and 21st century architecture and town planning with special focus on
+            cultural, social, political and economic contexts as well as at the evaluation, documentation
+            and restoration of modern movement buildings and sites. During the last years special attention
+            was paid to the impact of totalitarian and authoritarian regimes on architecture, to the modern
+            architecture and town planning concepts and their coexistence and conflicts with traditional town
+            and to the role of women in 20th century architecture. The Department of architecture collaborates
+            regularly with the Monuments Board of the Slovak republic and with the DOCOMOMO International.'
+    ]
+];
