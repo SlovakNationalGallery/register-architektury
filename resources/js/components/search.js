ddbfce178af3a37a2568ac8676c77d8c33700154
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-08-06 18:57:58 +0200

    code cleanup

--- resources/js/components/search.js
+++ resources/js/components/search.js
@@ -2,86 +2,85 @@ Bloodhound = require('corejs-typeahead/dist/bloodhound.js');
 require('corejs-typeahead/dist/typeahead.jquery.js');
 
 var architects = new Bloodhound({
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/api/architekti/suggest/?search=%QUERY',
-    wildcard: '%QUERY',
-    filter: function (architects) {
-            return $.map(architects.results, function (item) {
-                return {
-                    url: item.url,
-                    value: item.name
-                };
-            });
-        }
-    }
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	datumTokenizer: Bloodhound.tokenizers.whitespace,
+	remote: {
+		url: '/api/architekti/suggest/?search=%QUERY',
+		wildcard: '%QUERY',
+		filter: function (architects) {
+			return $.map(architects.results, function (item) {
+				return {
+					url: item.url,
+					value: item.name
+				};
+			});
+		}
+	}
 });
 
 var buildings = new Bloodhound({
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/api/objekty/suggest/?search=%QUERY',
-    wildcard: '%QUERY',
-    filter: function (buildings) {
-            return $.map(buildings.results, function (item) {
-                return {
-                    url: item.url,
-                    architects: item.architects,
-                    title: item.title
-                    // image: building.image,
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	datumTokenizer: Bloodhound.tokenizers.whitespace,
+	remote: {
+		url: '/api/objekty/suggest/?search=%QUERY',
+		wildcard: '%QUERY',
+		filter: function (buildings) {
+			return $.map(buildings.results, function (item) {
+				return {
+					url: item.url,
+					architects: item.architects,
+					title: item.title
                 };
             });
-        }
-    }
+		}
+	}
 });
 
 
 $("document").ready(function() {
 
-  buildings.initialize();
-  architects.initialize();
+	buildings.initialize();
+	architects.initialize();
 
-  $search = $('#search');
+	$search = $('#search');
 
-  $search.typeahead(
-  {
-    hint: false,
-    highlight: true,
-    minLength: 2
-  },
-  {
-    name: 'architects',
-    displayKey: 'value',
-    limit: 3,
-    source: architects.ttAdapter(),
-    templates: {
-        header: '<h3 class="suggest-type-name my-0 pt-3 pb-1 ls-3">' + $search.data('architects-title') + '</h3>',
-        suggestion: function (data) {
-          return '<div>'+data.value+'</div>';
-        }
-    }
-  },
-  {
-    name: 'buildings',
-    displayKey: 'value',
-    limit: 4,
-    source: buildings.ttAdapter(),
-    templates: {
-      header: '<h3 class="suggest-type-name my-0 pt-3 pb-1 ls-3">'+ $search.data('objects-title') +'</h3>',
-      suggestion: function (data) {
-          return '<div>' + data.title + '<br><span class="">' + data.architects + '</span></div>';
-      }
-    }
-  }).bind("typeahead:select", function(event, object, name) {
-      if (object.hasOwnProperty('url')) {
-          window.location.href = object.url;
-          return;
-      }
-      $(this).parents('form:first').submit();
-  });
+	$search.typeahead(
+	{
+		hint: false,
+		highlight: true,
+		minLength: 2
+	},
+	{
+		name: 'architects',
+		displayKey: 'value',
+		limit: 3,
+		source: architects.ttAdapter(),
+		templates: {
+			header: '<h3 class="suggest-type-name my-0 pt-3 pb-1 ls-3">' + $search.data('architects-title') + '</h3>',
+			suggestion: function (data) {
+				return '<div>'+data.value+'</div>';
+			}
+		}
+	},
+	{
+		name: 'buildings',
+		displayKey: 'value',
+		limit: 4,
+		source: buildings.ttAdapter(),
+		templates: {
+			header: '<h3 class="suggest-type-name my-0 pt-3 pb-1 ls-3">'+ $search.data('objects-title') +'</h3>',
+			suggestion: function (data) {
+				return '<div>' + data.title + '<br><span class="">' + data.architects + '</span></div>';
+			}
+		}
+	}).bind("typeahead:select", function(event, object, name) {
+		if (object.hasOwnProperty('url')) {
+			window.location.href = object.url;
+			return;
+		}
+		$(this).parents('form:first').submit();
+	});
 
-  $search.focus();
+	$search.focus();
 
 });
