1115c1b3d1afb7a8e073f5dd9a3190d92cecdacb
Ernest Walzel <ernest.walzel@sng.sk>
2020-08-11 15:53:03 +0200

    buildings-carousel component

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -45,3 +45,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+window.Flickity = require('flickity')
