7b013279c062ce37ae69759631a38115a121cb5c
Ernest Walzel <ernest.walzel@sng.sk>
2020-08-20 12:17:28 +0200

    Create Project resource (manageable from Backpack)

---
+++ database/migrations/2020_08_20_121046_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->json('title');
+            $table->string('slug')->unique();
+            $table->json('content')->nullable();
+            $table->timestamp('published_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
