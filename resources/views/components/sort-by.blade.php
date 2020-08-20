353617b700db7683aa0209244e3e6abd8dd56dde
Ernest Walzel <ernest.walzel@sng.sk>
2020-08-20 09:40:21 +0200

    Show relevance sorting based on default sort

--- resources/views/components/sort-by.blade.php
+++ resources/views/components/sort-by.blade.php
@@ -2,13 +2,14 @@
 // Set $default_sort to highlight implicit sorting
 $current_sort = request('sort_by') ?? $default_sort ?? null;
 
+$enable_relevance_sort = $default_sort == 'relevance';
 $chronological_sort = $current_sort == 'newest' ? 'oldest' : 'newest';
 $alphabetical_sort  = $current_sort == 'name_asc' ? 'name_desc' : 'name_asc';
 @endphp
 
 <span class="d-block d-sm-inline mb-2 mb-sm-0 text-nowrap mr-0 mr-md-3 mr-lg-5">{{ $count }} {{ trans_choice($count_translation_key, $count) }}</span>
 
-@if (request()->filled('search'))
+@if ($enable_relevance_sort)
     <a href="{{ request()->fullUrlWithQuery(['sort_by' => '']) }}" class="link-no-underline border-0 text-nowrap mr-2 mr-md-3 mr-lg-5">
         {{ __('filters.sort_by.relevance') }}
         <span class="icon-arrow-down {{ $current_sort == 'relevance' ? '' : 'text-secondary' }}"></span>
