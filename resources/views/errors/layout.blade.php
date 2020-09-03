d6e6b21d752faf2508753fc577a32f30206c6380
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-03 17:08:18 +0200

    move css from <style> to _type.scss

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -6,27 +6,13 @@
 
 @section('content')
 
-  <style>
-    .error_number {
-      font-size: 3rem;
-      font-weight: 500;
-      line-height: 3rem;
-      margin-top: 100px;
-    }
-    .error_number small {
-      font-size: 3rem;
-      font-weight: 500;
-    }
-
-  </style>
-
 @include('components.header')
 
 <div class="container-fluid p-0 bg-light h-100">
     <div class="row no-gutters h-100">
       <div class="col-md-12 text-center">
         <div class="error_number ls-3">
-          <small>ERROR</small><br>
+          ERROR<br>
           {{ $error_number }}
         </div>
         <div class="error_title my-4">
