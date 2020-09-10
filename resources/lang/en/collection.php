ce3bd76f168ae1fa259052644f51414b1a132566
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-10 16:00:03 +0200

    Add translations for links

--- resources/lang/en/collection.php
+++ resources/lang/en/collection.php
@@ -2,4 +2,6 @@
 
 return [
     'count' => '{0} collections|[1] collection|[2,*] collections',
+    'go_to_project' => 'See project information',
+    'go_to_collection' => 'Show objects in collection',
 ];
