0727929de8add6ecf9de2fc1950398e59223833e
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-11 11:26:36 +0200

    use GeoJSON source for markers in map.js

--- resources/js/components/map.js
+++ resources/js/components/map.js
@@ -33,14 +33,83 @@ function initMap() {
 		zoom: zoom
 	});
 
-	$.getJSON('/api/markers', function(data) {
-        data.features.forEach(function(marker) {
-        	var is_active = (marker.properties.id == active_id) ? 1 : 0;
-        	if (marker.geometry.coordinates) {
-        		initMarker(marker, map, is_active);
-        	}
-        });
-    });
+	// map.addControl(new mapboxgl.NavigationControl());
+
+	// $.getJSON('/api/markers', function(data) {
+    //     data.features.forEach(function(marker) {
+    //     	var is_active = (marker.properties.id == active_id) ? 1 : 0;
+    //     	initMarker(marker, map, is_active);
+    //     });
+    // });
+
+	map.on('load', function () {
+
+	 	// Add a GeoJSON source with 2 points
+	 	map.addSource('points', {
+	 		'type': 'geojson',
+	 		'data': '/api/markers',
+	 		'cluster': true,
+	 		'clusterMaxZoom': 14, // Max zoom to cluster points on
+	 		'clusterRadius': 50 // Radius of each cluster when clustering points (defaults to 50)
+		});
+
+		map.addLayer({
+			id: 'clusters',
+			type: 'circle',
+			source: 'points',
+			filter: ['has', 'point_count'],
+			paint: {
+				// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+				// with three steps to implement three types of circles:
+				//   * Blue, 20px circles when point count is less than 100
+				//   * Yellow, 30px circles when point count is between 100 and 750
+				//   * Pink, 40px circles when point count is greater than or equal to 750
+				'circle-color': [
+					'step',
+					['get', 'point_count'],
+					'#51bbd6',
+					10,
+					'#f1f075',
+					50,
+					'#f28cb1'
+				],
+				'circle-radius': [
+					'step',
+					['get', 'point_count'],
+					20,
+					10,
+					30,
+					50,
+					40
+				]
+			}
+		});
+
+		map.addLayer({
+			id: 'cluster-count',
+			type: 'symbol',
+			source: 'points',
+			filter: ['has', 'point_count'],
+			layout: {
+				'text-field': '{point_count_abbreviated}',
+				'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+				'text-size': 12
+			}
+		});
+
+		map.addLayer({
+			id: 'unclustered-point',
+			type: 'circle',
+			source: 'points',
+			filter: ['!', ['has', 'point_count']],
+			paint: {
+				'circle-color': '#11b4da',
+				'circle-radius': 7,
+				'circle-stroke-width': 1,
+				'circle-stroke-color': '#fff'
+			}
+		});
+ 	});
 }
 
 $(document).ready(function(){
