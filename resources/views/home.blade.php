335424eaa0bfb733b6eefefb5a3a028a3c5c2b7d
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-16 14:56:42 +0200

    display featured description with tags and sorting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,13 +20,37 @@
 ])
 
 <div class="container-fluid p-0">
-    <div class="row px-3">
-        <div class="col-md-12 p-3">
+    <div class="row no-gutters">
+        <div class="col-md-12 p-3" id="filters">
+            {{-- filters --}}
+            <form action="{{ route('building.index') }}">
+                <div class="row no-gutters align-items-end">
+                    <div class="col-md-6 p-3">
+                        @if($featured_filter)
+                            <p>{{ $featured_filter->description }}</p>
+
+                            @foreach ($featured_filter->tags as $filter)
+                                @include('components.active-filter', [
+                                    'label' => $filter,
+                                    'name' => 'filters[]',
+                                    'value' => $filter,
+                                ])
+                            @endforeach
+                        @endif
+                    </div>
+                    <div class="col-md-6 p-3 pt-4 text-left text-sm-right">
+                        @include('components.sort-by', [
+                            'count' => $buildings->total(),
+                            'count_translation_key' => 'building.count',
+                            'default_sort' => (request()->filled('search')) ? 'relevance' : 'name_asc',
+                        ])
+                    </div>
+                </div>
+
+            </form>
+            {{-- /filters --}}
+
             <div class="row items px-3">
-                {{-- TODO --}}
-                @if($featured_filter)
-                {{ $featured_filter->description }}
-                @endif
                 @foreach ($buildings as $i=>$building)
                     <div class="col-lg-3 col-sm-6 d-flex align-items-stretch item">
                         @include('components.building-card', ['building' => $building])
@@ -41,4 +65,5 @@
         </div>
     </div>
 </div>
+
 @endsection
