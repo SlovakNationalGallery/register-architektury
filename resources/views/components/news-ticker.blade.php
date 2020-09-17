6f2acf5b958a142c7fb543fd926dfc6dfc3511b5
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-17 15:50:54 +0200

    newsticker with article data

--- resources/views/components/news-ticker.blade.php
+++ resources/views/components/news-ticker.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid p-0 pt-1 bg-light border-bottom" id="news">
     <marquee class="news-scroll py-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
-        <a href="">Stanovisko k návrhu na vydanie osvedčenia o významnej investícii spoločnosti Smart city s.r.o. A spoločnosti J & T REAL ESTATE</a>
-        <a href="">Stanovisko k návrhu na vydanie osvedčenia o významnej investícii spoločnosti Smart city s.r.o. A spoločnosti J & T REAL ESTATE</a>
-        <a href="">Stanovisko k návrhu na vydanie osvedčenia o významnej investícii spoločnosti Smart city s.r.o. A spoločnosti J & T REAL ESTATE</a>
+        @foreach (\App\Models\Article::published()->latest()->take(3)->get() as $article)
+            <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
+        @endforeach
     </marquee>
 </div>
