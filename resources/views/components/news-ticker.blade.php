ca489c85d65a164dce81a42d3d82594aadf3a1c3
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-17 15:52:24 +0200

    show newsticker only if has published articles

--- resources/views/components/news-ticker.blade.php
+++ resources/views/components/news-ticker.blade.php
@@ -1,7 +1,13 @@
-<div class="container-fluid p-0 pt-1 bg-light border-bottom" id="news">
-    <marquee class="news-scroll py-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
-        @foreach (\App\Models\Article::published()->latest()->take(3)->get() as $article)
-            <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
-        @endforeach
-    </marquee>
-</div>
+@php
+    $articles = \App\Models\Article::published()->latest()->take(3)->get();
+@endphp
+
+@if ($articles->count() > 0)
+    <div class="container-fluid p-0 pt-1 bg-light border-bottom" id="news">
+        <marquee class="news-scroll py-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
+            @foreach ($articles as $article)
+                <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
+            @endforeach
+        </marquee>
+    </div>
+@endif
