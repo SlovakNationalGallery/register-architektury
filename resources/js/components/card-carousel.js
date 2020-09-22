6b69536bfb4427f113f18953443a391c7a0cf2df
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-22 23:18:14 +0200

    add + style prev/next buttons

--- resources/js/components/card-carousel.js
+++ resources/js/components/card-carousel.js
@@ -1,9 +1,9 @@
 require('flickity-as-nav-for');
 require('flickity-fullscreen');
 
-$('.card-carousel-main').each(function(i, obj) {
-    // const nextButton = $(obj).querySelector('.next-button')
-    // const prevButton = $(obj).querySelector('.prev-button')
+$('.card-carousel').each(function(i, obj) {
+    const nextButton = $(obj).find('.next-button')[ 0 ];
+    const prevButton = $(obj).find('.prev-button')[ 0 ];
 
     // Initialize main carousel
     new Flickity(obj, {
@@ -11,6 +11,20 @@ $('.card-carousel-main').each(function(i, obj) {
         pageDots: false,
         lazyLoad: 2,
         fullscreen: false,
-        cellSelector: '.carousel-cell'
+        cellSelector: '.carousel-cell',
+
+        on: {
+            ready() {
+                nextButton.onclick = this.next.bind(this)
+                prevButton.onclick = this.previous.bind(this)
+
+                nextButton.disabled = this.selectedIndex === this.slides.length - 1 || this.slides.length === 0
+                prevButton.disabled = this.selectedIndex === 0
+            },
+            change(event, index) {
+                nextButton.disabled = this.selectedIndex === this.slides.length - 1 || this.slides.length === 0
+                prevButton.disabled = this.selectedIndex === 0
+            }
+        }
     });
 });
--- resources/views/components/card-carousel.blade.php
+++ resources/views/components/card-carousel.blade.php
@@ -1,13 +1,14 @@
 @if ($images->isEmpty())
-    <div class="overflow-hidden vh-38 mb-2">
+    <div class="vh-38 mb-2">
         <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
             <img src="{{ asset('images/no-image-building.svg') }}" class="mh-100 mw-100 d-block mx-auto my-auto" alt="">
         </div>
     </div>
 @else
-<div class="card-carousel-main overflow-hidden vh-38 mb-2">
+<div class="card-carousel overflow-y-hidden vh-38 mb-2">
+    <button class="prev-button btn btn-lg px-2 icon-chevron-left" disabled></button>
     @foreach($images as $index => $image)
-    <div class="w-100 d-flex flex-column justify-content-center align-items-center carousel-cell">
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center carousel-cell">
         <img
             data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
             data-flickity-lazyload-src="{{ $image->getUrl() }}"
@@ -16,8 +17,8 @@
         >
     </div>
     @endforeach
-    {{-- <button class="next-button btn btn-lg icon-chevron-right mr-1"></button> --}}
-    {{-- <button class="prev-button btn btn-lg icon-chevron-left ml-1"></button> --}}
+
+    <button class="next-button btn btn-lg px-2 icon-chevron-right" disabled></button>
 </div>
 
 @endif
