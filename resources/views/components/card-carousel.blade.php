c8300055fe636b6e5af857cedfa5ffa667f69717
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-24 12:32:41 +0200

    Fix images for card-carousel

--- resources/views/components/card-carousel.blade.php
+++ resources/views/components/card-carousel.blade.php
@@ -6,18 +6,17 @@
     </div>
 @else
 <div class="card-carousel vh-38 mb-2">
-    <button class="prev-button btn btn-lg px-2 icon-chevron-left" disabled></button>
     @foreach($images as $index => $image)
-    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center carousel-cell">
+    <div class="w-100 h-100 d-flex justify-content-center align-items-center carousel-cell">
         <img
             data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
             data-flickity-lazyload-src="{{ $image->getUrl() }}"
-            sizes="(min-width: 768px) 40vw, 100vw"
-            class="mh-100 mw-100 d-block mx-auto my-0"
+            sizes="1px" {{-- Initial size updated in JS --}}
+            class="mh-100 align-self-center"
         >
     </div>
     @endforeach
-
+    <button class="prev-button btn btn-lg px-2 icon-chevron-left" disabled></button>
     <button class="next-button btn btn-lg px-2 icon-chevron-right" disabled></button>
 </div>
 
