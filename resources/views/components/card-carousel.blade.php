ab6c75852cd7ebffc346b9ff205ccab968f860b3
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-24 13:56:19 +0200

    Fix outer margins for building-cards

--- resources/views/components/card-carousel.blade.php
+++ resources/views/components/card-carousel.blade.php
@@ -5,19 +5,18 @@
         </div>
     </div>
 @else
-<div class="card-carousel vh-38 mb-2">
-    @foreach($images as $index => $image)
-    <div class="w-100 h-100 d-flex justify-content-center align-items-center carousel-cell">
-        <img
-            data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
-            data-flickity-lazyload-src="{{ $image->getUrl() }}"
-            sizes="1px" {{-- Initial size updated in JS --}}
-            class="mh-100 align-self-center"
-        >
+    <div class="card-carousel vh-38 mb-2">
+        @foreach($images as $index => $image)
+        <div class="w-100 h-100 d-flex justify-content-center align-items-center carousel-cell">
+            <img
+                data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
+                data-flickity-lazyload-src="{{ $image->getUrl() }}"
+                sizes="1px" {{-- Initial size updated in JS --}}
+                class="mh-100 align-self-center"
+            >
+        </div>
+        @endforeach
+        <button class="prev-button btn btn-lg px-2 icon-chevron-left" disabled></button>
+        <button class="next-button btn btn-lg px-2 icon-chevron-right" disabled></button>
     </div>
-    @endforeach
-    <button class="prev-button btn btn-lg px-2 icon-chevron-left" disabled></button>
-    <button class="next-button btn btn-lg px-2 icon-chevron-right" disabled></button>
-</div>
-
 @endif
