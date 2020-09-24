ea148e14d7a5a6fa814ab273f7b1b19861f86e08
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-24 14:21:55 +0200

    Make building card carousel clickable

--- resources/views/components/building-card.blade.php
+++ resources/views/components/building-card.blade.php
@@ -1,27 +1,29 @@
 @php
-$images = $building->processedImages->map(fn ($image) => $image->getFirstMedia());
+$images = $building->processedImages->map->getFirstMedia();
 @endphp
 <div class="building-card card my-4 border">
+    <div class="vh-38 p-4">
     @if ($images->isEmpty())
-        <div class="vh-38 m-4 d-flex flex-column justify-content-center align-items-center">
+        <div class="h-100 d-flex flex-column justify-content-center align-items-center">
             <img src="{{ asset('images/no-image-building.svg') }}" class="w-100" alt="">
         </div>
     @else
-        <div class="card-carousel vh-38 m-4">
+        <div class="card-carousel h-100">
             @foreach($images as $index => $image)
-            <div class="w-100 h-100 d-flex justify-content-center align-items-center carousel-cell mx-3">
+            <a href="{{ $building->url }}" class="carousel-cell w-100 h-100 d-flex justify-content-center align-items-center mx-3">
                 <img
                     data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
                     data-flickity-lazyload-src="{{ $image->getUrl() }}"
                     sizes="1px" {{-- Initial size updated in JS --}}
                     class="mw-100 mh-100"
                 >
-            </div>
+            </a>
             @endforeach
             <button class="prev-button btn btn-lg icon-chevron-left" disabled></button>
             <button class="next-button btn btn-lg icon-chevron-right" disabled></button>
         </div>
     @endif
+    </div>
     <div class="card-body ls-1 d-flex flex-column justify-content-between px-4 pb-4 pt-0">
         @include('components.tags', ['tags' => $building->tags])
 
