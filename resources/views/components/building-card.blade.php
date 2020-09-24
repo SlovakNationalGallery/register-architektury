a00e2af57e31daf4ec99b0421d5ae9be8a2bdd4e
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-24 13:56:19 +0200

    Fix outer margins for building-cards

--- resources/views/components/building-card.blade.php
+++ resources/views/components/building-card.blade.php
@@ -1,16 +1,31 @@
-<div class="card my-4 border">
-    <div class="card-body ls-1 pb-1">
-        {{-- <a href="{{ $building->url }}" class="image-link"> --}}
-            @include('components.card-carousel', [
-                'images' => $building->processedImages->map(fn ($image) => $image->getFirstMedia())
-            ])
-        {{-- </a> --}}
-        
+@php
+$images = $building->processedImages->map(fn ($image) => $image->getFirstMedia());
+@endphp
+<div class="building-card card my-4 border">
+    @if ($images->isEmpty())
+        <div class="vh-38 m-4 d-flex flex-column justify-content-center align-items-center">
+            <img src="{{ asset('images/no-image-building.svg') }}" class="w-100" alt="">
+        </div>
+    @else
+        <div class="card-carousel vh-38 m-4">
+            @foreach($images as $index => $image)
+            <div class="w-100 h-100 d-flex justify-content-center align-items-center carousel-cell mx-3">
+                <img
+                    data-flickity-lazyload-srcset="{{ $image->getSrcset() }}"
+                    data-flickity-lazyload-src="{{ $image->getUrl() }}"
+                    sizes="1px" {{-- Initial size updated in JS --}}
+                    class="mw-100 mh-100"
+                >
+            </div>
+            @endforeach
+            <button class="prev-button btn btn-lg icon-chevron-left" disabled></button>
+            <button class="next-button btn btn-lg icon-chevron-right" disabled></button>
+        </div>
+    @endif
+    <div class="card-body ls-1 d-flex flex-column justify-content-between px-4 pb-4 pt-0">
         @include('components.tags', ['tags' => $building->tags])
 
-    </div>
-    <div class="card-footer bg-white ls-1 border-top-0 py-1">
-        <p class="card-text">
+        <p class="card-text pt-4">
             <a href="{{ $building->url }}" class="link-no-underline">{{ $building->title }}</a>
         </p>
     </div>
