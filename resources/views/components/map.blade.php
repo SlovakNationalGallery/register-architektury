25ed0843d41394b7b3461d7f380f769372c213e7
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-24 12:02:19 +0200

    map toggle button smaller

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -8,7 +8,7 @@ $show_map = session('show_map', true);
     <div class="position-absolute bg-light border-bottom vh-38 w-100 {{ ($show_map) ? '' : 'hide' }}" id="map" data-center="{{ json_encode($center) }}" data-zoom="{{ $zoom }}" data-active_id="{{ $active_id ?? 0 }}"></div>
     <div class="row no-gutters">
     	<div class="col-12 text-right z-index-1 p-3">
-            <button class="btn btn-outline-dark btn-lg mb-2 text-uppercase {{ ($show_map) ? 'active' : '' }}" id="map-toggle">{{ __('map.map') }}</button>
+            <button class="btn btn-outline-dark ls-1 mb-2 text-uppercase {{ ($show_map) ? 'active' : '' }}" id="map-toggle">{{ __('map.map') }}</button>
      	</div>
 	</div>
 </div>
