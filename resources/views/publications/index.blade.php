e0b58df5f791f9573b7d38d943d836b2cb0547bc
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-09-24 12:26:47 +0200

    fix publications

--- resources/views/publications/index.blade.php
+++ resources/views/publications/index.blade.php
@@ -16,24 +16,24 @@
         </div>
     </div>
 </div>
-<div class="container-fluid pb-6">
+<div class="container-fluid pb-6 border-top">
     @foreach($publications as $publication)
-    <div class="row border-top py-3">
+    <div class="row border-bottom py-3">
         <div class="col-lg-3">
             <div class="row pb-4 pb-lg-0">
                 <div class="col-6">
                     <img src="{{ Storage::url($publication->cover_image) }}" class="mw-100">
                 </div>
-                <div class="col-6 d-flex flex-column justify-content-end">
+                <div class="col-6 d-flex flex-column">
                     <h6 class="mb-4">{{ $publication->title }}</h6>
                     {{ $publication->authors }}
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 mb-4 mb-lg-0 d-flex flex-column justify-content-end last-child-mb-0">
+        <div class="col-lg-6 mb-4 mb-lg-0 d-flex flex-column last-child-mb-0">
             {!! $publication->description !!}
         </div>
-        <div class="col-lg-3 d-flex flex-column justify-content-end">
+        <div class="col-lg-3 d-flex flex-column">
             @isset($publication->issuu_url)
             <a href="{{ $publication->issuu_url }}">
                 <span class="align-bottom">{{ __('about.publications.issuu_link') }} ↪</span>
