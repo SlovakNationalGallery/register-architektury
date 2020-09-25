9b7ce6bbb6d7f05273a0e6c2141ec31099cb570e
Ernest Walzel <ernest.walzel@sng.sk>
2020-09-25 13:41:22 +0200

    Handle legacy /architekti/ routes

--- app/Http/Controllers/LegacyRedirectController.php
+++ app/Http/Controllers/LegacyRedirectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Architect;
 use App\Models\Building;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -15,5 +16,20 @@ class LegacyRedirectController extends Controller
 
         if ($building) return redirect($building->url);
         return redirect(route('building.index', ['search' => $title]));
+    }
+
+    public function showArchitect(string $oldId, string $slug)
+    {
+        $allNames = Str::of($slug)->explode('-');
+        $lastName = $allNames->shift();
+        $firstNames = $allNames->join(' ');
+
+        $architect = Architect::search('*')
+            ->whereMatch('first_name.folded', $firstNames)
+            ->whereMatch('last_name.folded', $lastName)
+            ->first();
+
+        if ($architect) return redirect(route('architects.show', $architect));
+        return redirect(route('architects.index', ['search' => "$lastName $firstNames"]));
     }
 }
