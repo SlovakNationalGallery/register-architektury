2bab427863a0e23041b0da7ea793352464c9d869
Ernest Walzel <ernest.walzel@sng.sk>
2020-10-02 11:07:00 +0200

    Terminate Horizon with a 'sudo'

--- deployer/custom.php
+++ deployer/custom.php
@@ -7,3 +7,7 @@ task('elastic:migrate', function () {
     run('{{bin/php}} {{release_path}}/artisan regarch:elastic:migrate -q');
 });
 
+desc('Terminate horizon as user www-data');
+task('sudo:artisan:horizon:terminate', function () {
+    run('sudo -u www-data {{bin/php}} {{release_path}}/artisan horizon:terminate');
+});
