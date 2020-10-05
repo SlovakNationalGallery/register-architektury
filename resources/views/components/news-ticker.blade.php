f47aaa1d06d7b0a7e4960bfa824377eb09492370
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-10-05 22:46:50 +0200

    newsticker with gradient and close button

--- resources/views/components/news-ticker.blade.php
+++ resources/views/components/news-ticker.blade.php
@@ -3,11 +3,12 @@
 @endphp
 
 @if ($articles->count() > 0)
-    <div class="container-fluid p-0 pt-1 bg-light border-bottom" id="news">
+    <div class="container-fluid p-0 pt-1 bg-light border-bottom d-flex" id="news">
         <marquee class="news-scroll py-2 ls-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
             @foreach ($articles as $article)
                 <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
             @endforeach
         </marquee>
+        <a href="#close" class="d-block icon-close p-2 link-no-underline border-bottom-0"></a>
     </div>
 @endif
