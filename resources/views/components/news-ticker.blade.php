5876220bfde4452fd429ef3974cfeefe05eb742b
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-10-09 14:28:08 +0200

    fix gradient for ios safari in news-ticker

--- resources/views/components/news-ticker.blade.php
+++ resources/views/components/news-ticker.blade.php
@@ -4,11 +4,13 @@
 
 @if ($articles->count() > 0)
     <div class="container-fluid p-0 pt-1 bg-light border-bottom" id="news">
-        <marquee class="news-scroll py-2 ls-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
-            @foreach ($articles as $article)
-                <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
-            @endforeach
-        </marquee>
+        <div class="conainter-marquee">
+            <marquee class="news-scroll py-2 ls-2" behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
+                @foreach ($articles as $article)
+                    <a href="{{ route('about.articles.show', $article) }}" class="link-no-underline mr-3">{{ $article->title }}</a>
+                @endforeach
+            </marquee>
+        </div>
         <a href="#close" class="d-block icon-close p-2 link-no-underline border-bottom-0" id="hide-news"></a>
     </div>
 @endif
