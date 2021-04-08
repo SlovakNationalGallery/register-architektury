13d608e6c5201e5e1a095303f1ec9eb284f53d25
Ernest Walzel <ernest.walzel@sng.sk>
2021-04-08 14:26:51 +0200

    Cache menu items

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\HasTranslations;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class Setting extends Model {
     use CrudTrait, HasTranslations;
@@ -28,18 +29,36 @@ class Setting extends Model {
         'menu_item_4_title',
     ];
 
-    public function getMenuItemsAttribute()
+    protected static function booted()
     {
-        //TODO cache this query
-        $featuredProjects = Project::whereIn('id', [
-            $this->menu_item_1_project_id,
-            $this->menu_item_2_project_id,
-            $this->menu_item_3_project_id,
-            $this->menu_item_4_project_id,
-        ])->get();
+        static::updated(function () {
+            Cache::forget('settings.current');
+            Cache::forget('settings.menu_projects');
+        });
+    }
+
+    public static function menuItems()
+    {
+        $setting = Cache::rememberForever('settings.current', function () {
+            return self::first();
+        });
+
+        return $setting->getMenuItems();
+    }
+
+    private function getMenuItems()
+    {
+        $featuredProjects = Cache::rememberForever('settings.menu_projects', function () {
+            return Project::whereIn('id', [
+                $this->menu_item_1_project_id,
+                $this->menu_item_2_project_id,
+                $this->menu_item_3_project_id,
+                $this->menu_item_4_project_id,
+            ])->get();
+        });
 
         return Collection::times(4, function ($number) use ($featuredProjects) {
-            $projectId = $this->{"menu_item_" . $number . "_project_id"};
+            $projectId = $this->{"menu_item_{$number}_project_id"};
             $project = $featuredProjects->firstWhere('id', $projectId);
 
             return (object) [
