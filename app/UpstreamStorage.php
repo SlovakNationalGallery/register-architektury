88c4912611997da200545ede3532f2e2ead36d72
Ernest Walzel <ernest.walzel@sng.sk>
2023-02-24 15:15:20 +0100

    Recognize 'S:/AA20/OddArch/AA20 dáta' upstream storage path

--- app/UpstreamStorage.php
+++ app/UpstreamStorage.php
@@ -14,11 +14,15 @@ class UpstreamStorage
         $normalizedPath = Str::of($sourcePath)->replace('\\', '/');
 
         if (Str::startsWith($normalizedPath, '//AA20/OddArch/AA20 dáta/')) {
-            return $storage->get($normalizedPath->replaceFirst('//AA20/OddArch/AA20 dáta/',''));
+            return $storage->get($normalizedPath->replaceFirst('//AA20/OddArch/AA20 dáta/', ''));
         }
 
         if (Str::startsWith($normalizedPath, 'S:/AA20 dáta/')) {
-            return $storage->get($normalizedPath->replaceFirst('S:/AA20 dáta/',''));
+            return $storage->get($normalizedPath->replaceFirst('S:/AA20 dáta/', ''));
+        }
+
+        if (Str::startsWith($normalizedPath, 'S:/AA20/OddArch/AA20 dáta/')) {
+            return $storage->get($normalizedPath->replaceFirst('S:/AA20/OddArch/AA20 dáta/', ''));
         }
 
         throw new Exception("Unrecognized source path $sourcePath");
